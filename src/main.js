839321e976c525946a22a4446af96a81b0faf11c
Yura <yurii_kulachenko@mail.ru>
2020-08-07 21:14:50 +0300

    ДОбавляет перечисление для кол-ва задача и количества задач выводимых за шаг

--- src/main.js
+++ src/main.js
@@ -8,10 +8,12 @@ import {createLoadButtonTemplate} from './view/load-button.js';
 import {generateTask} from './mock/task.js';
 import {generateFilter} from './mock/filter.js';
 
-const TASK_COUNT = 20;
-const TASK_COUNT_PER_STEP = 8;
+const CountType = {
+  TASK_COUNT: 20,
+  TASK_COUNT_PER_STEP: 8,
+};
 
-const tasks = new Array(TASK_COUNT).fill().map(generateTask);
+const tasks = new Array(CountType.TASK_COUNT).fill().map(generateTask);
 const filters = generateFilter(tasks);
 
 const mainElement = document.querySelector(`.main`);
@@ -30,11 +32,11 @@ const boardTasksElement = boardElement.querySelector(`.board__tasks`);
 
 render(boardElement, createSortingTemplate(), `afterbegin`);
 render(boardTasksElement, createEditTaskTemplate(tasks[0]), `beforeend`);
-for (let x = 1; x < TASK_COUNT_PER_STEP; x++) {
+for (let x = 1; x < CountType.TASK_COUNT_PER_STEP; x++) {
   render(boardTasksElement, createTaskTemplate(tasks[x]), `beforeend`);
 }
-if (tasks.length > TASK_COUNT_PER_STEP) {
-  let renderedTaskCount = TASK_COUNT_PER_STEP;
+if (tasks.length > CountType.TASK_COUNT_PER_STEP) {
+  let renderedTaskCount = CountType.TASK_COUNT_PER_STEP;
 
   render(boardElement, createLoadButtonTemplate(), `beforeend`);
   const loadMoreElement = boardElement.querySelector(`.load-more`);
@@ -42,10 +44,10 @@ if (tasks.length > TASK_COUNT_PER_STEP) {
   loadMoreElement.addEventListener(`click`, (evt) => {
     evt.preventDefault();
     tasks
-      .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
+      .slice(renderedTaskCount, renderedTaskCount + CountType.TASK_COUNT_PER_STEP)
       .forEach((task) => render(boardTasksElement, createTaskTemplate(task), `beforeend`));
 
-    renderedTaskCount += TASK_COUNT_PER_STEP;
+    renderedTaskCount += CountType.TASK_COUNT_PER_STEP;
     if (renderedTaskCount >= tasks.length) {
       loadMoreElement.remove();
     }
