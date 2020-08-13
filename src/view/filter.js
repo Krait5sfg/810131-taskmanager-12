8b65d412bd2f91339d9e339004fd4e668956a607
Yura <yurii_kulachenko@mail.ru>
2020-08-13 22:35:39 +0300

    Абстрактный класс, наследование от него у всех классов

--- src/view/filter.js
+++ src/view/filter.js
@@ -1,5 +1,4 @@
-import {createElement} from "../utils.js";
-import {Number} from '../const.js';
+import AbstractView from "./abstract.js";
 
 const createFilterItemTemplate = (filter, isChecked) => {
   const {title, count} = filter;
@@ -10,7 +9,7 @@ const createFilterItemTemplate = (filter, isChecked) => {
 
 const createFilterTemplate = (filterItems) => {
   const filterItemsTemplate = filterItems
-    .map((filter, index) => createFilterItemTemplate(filter, index === Number.ZERO))
+    .map((filter, index) => createFilterItemTemplate(filter, index === 0))
     .join(``);
 
   return `<section class="main__filter filter container">
@@ -18,25 +17,13 @@ const createFilterTemplate = (filterItems) => {
     </section>`;
 };
 
-export default class Filter {
+export default class Filter extends AbstractView {
   constructor(filters) {
+    super();
     this._filters = filters;
-    this._element = null;
   }
 
   getTemplate() {
     return createFilterTemplate(this._filters);
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
